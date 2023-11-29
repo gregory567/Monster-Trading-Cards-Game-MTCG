987ac54f40782a84eb44fb48427395ca224199b5
peter <peti.tavaszi@gmail.com>
2023-11-29 19:23:40 +0100

    added the implementation of the card abstract class and updated the plantUML diagram

--- src/main/java/org/example/Card.java
+++ src/main/java/org/example/Card.java
@@ -1,13 +1,26 @@
 package org.example;
+import org.example.Specialty;
 
 public abstract class Card {
     protected String name;
-    protected static Integer damage;
+    protected Integer damage;
     protected String elementType;
+    protected Specialty[] specialties;
 
     public abstract void getAttributes();
 
     public abstract void displayCardInfo();
 
     public abstract void upgradeCard();
+
+    public void applySpecialty(Specialty specialty) {
+        if(specialties != null) {
+            for (Specialty cardSpecialty : specialties) {
+                if (cardSpecialty.getName().equals(specialty.getName())) {
+                    cardSpecialty.applySpecialtyEffect(this);
+                    // You might want to break here if each card can have only one instance of a specialty
+                }
+            }
+        }
+    }
 }
