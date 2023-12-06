7bea2e5d58bb7e259a27a3b50ead7ad69117de4f
peter <peti.tavaszi@gmail.com>
2023-12-06 15:44:21 +0100

    updated the gamecontroller class based on the plantuml diagram

--- src/main/java/org/example/GameController.java
+++ src/main/java/org/example/GameController.java
@@ -2,16 +2,28 @@ package org.example;
 
 import org.example.app.models.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameController {
-    private User[] users;
-    private Battle[] battles;
+    private List<User> users;
+    private List<Battle> battles;
 
-    public void createUser(String username, String password) {
+    public GameController() {
+        this.users = new ArrayList<>();
+        this.battles = new ArrayList<>();
+    }
 
+    public void createUser(String username, String password, double coins, Stack stack, Deck deck, Profile profile) {
+        // Implement the logic to create a new user
+        User newUser = new User(username, password, coins, stack, deck, profile);
+        users.add(newUser);
     }
 
     public void initiateBattle(User user1, User user2) {
-
+        // Implement the logic to initiate a battle between two users
+        Battle newBattle = new Battle(user1, user2);
+        battles.add(newBattle);
+        newBattle.startBattle();
     }
 }
-
