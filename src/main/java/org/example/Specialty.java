0d4c778b315c23f1aaca6d9bff9002ac5ad94f44
peter <peti.tavaszi@gmail.com>
2023-12-06 15:20:39 +0100

    updated class specialty

--- src/main/java/org/example/Specialty.java
+++ src/main/java/org/example/Specialty.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Specialty {
     private String name;
 
@@ -10,9 +15,53 @@ public class Specialty {
     public void applySpecialtyEffect(Card card) {
         // Implement the logic to apply the specialty effect to the card
         // This method should modify the card based on the specialty
-        // You can access the card's attributes using methods like card.getDamage(), card.getElementType(), etc.
-        // Modify the card accordingly based on the specialty logic.
-        // For example, you can increase or decrease damage, change element type, etc.
+
+        // Check the specialty type and apply the corresponding effect
+        switch (name) {
+            case "Goblin":
+                // Goblins are too afraid of Dragons to attack
+                if (card.getName().equals("Dragon")) {
+                    // set damage to 0
+                    card.setDamage(0);
+                }
+                break;
+
+            case "Wizzard":
+                // Wizzard can control Orks so they are not able to damage them
+                if (card.getName().equals("Ork")) {
+                    // set damage to 0
+                    card.setDamage(0);
+                }
+                break;
+
+            case "Knight":
+                // The armor of Knights is so heavy that WaterSpells make them drown instantly
+                if (card.getElementType() == ElementType.WATER) {
+                    // set damage to 0
+                    card.setDamage(0);
+                }
+                break;
+
+            case "Kraken":
+                // The Kraken is immune against spells
+                if (card instanceof SpellCard) {
+                    // set damage to 0
+                    card.setDamage(0);
+                }
+                break;
+
+            case "FireElves":
+                // The FireElves know Dragons since they were little and can evade their attacks
+                if (card.getName().equals("Dragon")) {
+                    // set damage to 0
+                    card.setDamage(0);
+                }
+                break;
+
+            default:
+                // Handle other specialties or no effect
+                break;
+        }
     }
 }
 
