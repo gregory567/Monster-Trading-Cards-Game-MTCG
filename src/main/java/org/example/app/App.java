50b55f67843ce37d960becdd850716e76c6299ba
peter <peti.tavaszi@gmail.com>
2023-12-08 14:57:53 +0100

    updated the app class

--- src/main/java/org/example/app/App.java
+++ src/main/java/org/example/app/App.java
@@ -1,6 +1,8 @@
 package org.example.app;
 
+import lombok.AllArgsConstructor;
 import lombok.Setter;
+import lombok.Getter;
 import lombok.AccessLevel;
 import org.example.app.controllers.UserController;
 import org.example.app.services.UserService;
@@ -10,8 +12,11 @@ import org.example.server.ServerApp;
 import org.example.http.ContentType;
 import org.example.http.HttpStatus;
 
+@AllArgsConstructor
+@Setter(AccessLevel.PRIVATE)
+@Getter(AccessLevel.PRIVATE)
 public class App implements ServerApp {
-    @Setter(AccessLevel.PRIVATE)
+
     private UserController userController;
 
     public App() {
@@ -20,13 +25,22 @@ public class App implements ServerApp {
 
     public Response handleRequest(Request request) {
 
-
-        switch (request.getMethod()) {
-            case GET: {
-                if (request.getPathname().equals("/users")) {
-                    return this.userController.getUsers();
+        try {
+            switch (request.getMethod()) {
+                case GET: {
+                    if (request.getPathname().equals("/users")) {
+                        return getUserController().getUsers();
+                    }
                 }
             }
+        } catch (Exception e) {
+            // Log the exception
+            e.printStackTrace();
+            return new Response(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    ContentType.JSON,
+                    "{ \"error\": \"Internal Server Error\", \"data\": null }"
+            );
         }
 
         return new Response(
