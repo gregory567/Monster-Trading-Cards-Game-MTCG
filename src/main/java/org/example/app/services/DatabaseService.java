a14508ba4d066291b04618b265929c5b7adf8ae4
peter <peti.tavaszi@gmail.com>
2023-12-11 11:55:14 +0100

    created class databaseservice

---
+++ src/main/java/org/example/app/services/DatabaseService.java
@@ -0,0 +1,26 @@
+package org.example.app.services;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseService {
+    private String connectionString = "jdbc:postgresql://localhost:5432/mtcgdb?user=postgres&password=postgres";
+    @Setter(AccessLevel.PRIVATE)
+    @Getter
+    private Connection connection;
+
+    public DatabaseService() {
+        try {
+            Connection connection = DriverManager.getConnection(connectionString);
+            setConnection(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
